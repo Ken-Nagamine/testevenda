e93feb719569004a33afbc5c8e27130340042e29
ken <ken@ken-System-Product-Name>
2019-09-12 15:17:41 -0300

    desenvolimento do model venda e criação da sql para inserir dados no h2 toda vez que atualizar...

--- src/main/java/com/ken/entity/Venda.java
+++ src/main/java/com/ken/entity/Venda.java
@@ -5,10 +5,83 @@
  */
 package com.ken.entity;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author ken
  */
-public class Venda {
+@Entity
+@Table(name="venda")
+public class Venda implements Serializable{
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private LocalDate data_venda;
+    
+    private BigDecimal valor;
+    
+    private int vendedor_id;
+    
+    private String vendedor_nome;
+
+    public Venda() {
+    }
+
+    public Venda(LocalDate data_venda, BigDecimal valor, int vendedor_id, String vendedor_nome) {
+        this.data_venda = data_venda;
+        this.valor = valor;
+        this.vendedor_id = vendedor_id;
+        this.vendedor_nome = vendedor_nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getData_venda() {
+        return data_venda;
+    }
+
+    public void setData_venda(LocalDate data_venda) {
+        this.data_venda = data_venda;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public int getVendedor_id() {
+        return vendedor_id;
+    }
+
+    public void setVendedor_id(int vendedor_id) {
+        this.vendedor_id = vendedor_id;
+    }
+
+    public String getVendedor_nome() {
+        return vendedor_nome;
+    }
+
+    public void setVendedor_nome(String vendedor_nome) {
+        this.vendedor_nome = vendedor_nome;
+    }
+   
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,26 @@
  * Created: 12/09/2019
  */
 
+INSERT INTO venda (data_venda, valor, vendedor_id, vendedor_nome) VALUES ('2019-01-01', '250.5', 1, 'Carlos');
+
+INSERT INTO venda (data_venda, valor, vendedor_id, vendedor_nome) VALUES ('2019-01-01', '100', 2, 'Marcos');
+
+INSERT INTO venda (data_venda, valor, vendedor_id, vendedor_nome) VALUES ('2019-01-01', '150', 1, 'Carlos');
+
+INSERT INTO venda (data_venda, valor, vendedor_id, vendedor_nome) VALUES ('2019-01-03', '120', 1, 'Carlos');
+
+INSERT INTO venda (data_venda, valor, vendedor_id, vendedor_nome) VALUES ('2019-01-03', '155.5', 3, 'Jorge');
+
+INSERT INTO venda (data_venda, valor, vendedor_id, vendedor_nome) VALUES ('2019-01-04', '125', 2, 'Marcos');
+
+INSERT INTO venda (data_venda, valor, vendedor_id, vendedor_nome) VALUES ('2019-01-04', '180', 2, 'Marcos');
+
+INSERT INTO venda (data_venda, valor, vendedor_id, vendedor_nome) VALUES ('2019-01-04', '230', 3, 'Jorge');
+
+INSERT INTO venda (data_venda, valor, vendedor_id, vendedor_nome) VALUES ('2019-01-04', '95', 3, 'Jorge');
+
+INSERT INTO venda (data_venda, valor, vendedor_id, vendedor_nome) VALUES ('2019-01-05', '222', 1, 'Carlos');
+
+INSERT INTO venda (data_venda, valor, vendedor_id, vendedor_nome) VALUES ('2019-01-05', '105', 2, 'Marcos');
+
+INSERT INTO venda (data_venda, valor, vendedor_id, vendedor_nome) VALUES ('2019-01-05', '145', 3, 'Jorge');
