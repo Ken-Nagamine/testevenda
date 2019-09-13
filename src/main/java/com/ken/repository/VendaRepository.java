e2594c7b4bb0a56ee362e5394347b4c448cec995
ken <ken@ken-System-Product-Name>
2019-09-13 08:20:27 -0300

    Query finalizada com retorno de nome_vendedor, quant_vendas, total_vendas(valor), e media das vendas diarias em função do periodo informado. Falta transformar a resposta em json...

--- src/main/java/com/ken/repository/VendaRepository.java
+++ src/main/java/com/ken/repository/VendaRepository.java
@@ -6,6 +6,8 @@
 package com.ken.repository;
 
 import com.ken.entity.Venda;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -20,4 +22,9 @@ public interface VendaRepository extends JpaRepository<Venda, Long>{
 
     @Query(value="select v.* from venda v where v.data_venda between ?1 and ?2", nativeQuery = true)
     List<Venda> buscarVendaComDataVendaNoIntervalo(String de, String ate);
+    
+    // retorna nome de vendedor,quant vendas, total vendas e média das vendas.
+    @Query(value="SELECT vendedor_nome as vendedor, COUNT(vendedor_id) as qtd_vendas , SUM(valor) as total_vendas, (sum(valor)/ count(vendedor_id)) as media_diaria from venda v where v.data_venda between ?1 and ?2 group by vendedor_nome", nativeQuery = true)
+    public ArrayList<Object> buscarVendedorTotalVendasMediaDiaria( String data_ini, String data_fim);
+   
 }
--- src/main/java/com/ken/resource/VendaResource.java
+++ src/main/java/com/ken/resource/VendaResource.java
@@ -8,6 +8,8 @@ package com.ken.resource;
 import com.ken.entity.Venda;
 import com.ken.repository.VendaRepository;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,11 +31,13 @@ public class VendaResource {
     @Autowired
     VendaRepository vendaRepository;
     
+    //consulta uma venda
     @GetMapping("/consultar_venda/{id}")
     public Venda listaVendaUnica(@PathVariable(value="id") long id){
         return vendaRepository.findById(id);
     }
     
+    // consulta todas as vendas..
     @GetMapping("/consultar_vendas")
     public List<Venda> listaVendas(){
         return vendaRepository.findAll();
@@ -46,10 +50,23 @@ public class VendaResource {
     }
     
     // Retorna as vendas em função de um periodo
+    // Para consultar ex: localhost:8080/api/consulta_venda_periodo?data_ini=2019-01-01&data_fim=2019-01-05
     @GetMapping("/consulta_venda_periodo")
     public List<Venda> listaVendaPeriodo(@RequestParam("data_ini") String data_ini, @RequestParam("data_fim") String data_fim){
         
         return vendaRepository.buscarVendaComDataVendaNoIntervalo(data_ini,data_fim);
+    }
+    
+    // Retorna nome, total_vendas e media_diaria em função de um periodo 
+    // Para consultar ex: localhost:8080/api/consulta_vendedor_periodo?data_ini=2019-01-01&data_fim=2019-01-05
+    @GetMapping("/consulta_vendedor_periodo")
+    public ArrayList<Object> listaVendedorPeriodo(@RequestParam("data_ini") String data_ini, @RequestParam("data_fim") String data_fim){
+        
+        //******************************************************************
+        // falta trasformar arraylist em json para retorno de informações..... 
+        
+        
+        return vendaRepository.buscarVendedorTotalVendasMediaDiaria(data_ini, data_fim);
     }
     
 }
